5901b85867635e5fca7143caf92d45ab6a077a56
unknown <mweinberg21@gmail.com>
2020-08-11 03:14:39 -0500

    Started log listing

--- src/containers/BuildProjectPage.jsx
+++ src/containers/BuildProjectPage.jsx
@@ -5,7 +5,7 @@ import Paper from '@material-ui/core/Paper'
 import PhasesList from '../components/BuildLog/PhasesList'
 import superagent from 'superagent'
 import { useParams } from 'react-router-dom'
-
+import moment from 'moment'
 
 
 const BuildProjectPage = () => {
@@ -19,23 +19,67 @@ const BuildProjectPage = () => {
 
 
 
-  const getHours = () => {
-    const entries = selectedPhase == 'all' ?
+  const getEntries = () => {
+    return selectedPhase == 'all' ?
       buildProjectData.phases.reduce((acc, x) => [ ...acc, ...x.entries ], [])
       : buildProjectData.phases.find(x => x.id === selectedPhase).entries
+  }
+
+
+
+  const getHeaderData = () => {
+    const phase = buildProjectData.phases.find(x => x.id === selectedPhase)
+
+    const complete = selectedPhase === 'all' ? buildProjectData.phases.reduce((acc, x) => !x ? false : acc, false) : phase.complete
+
+    const entries = selectedPhase == 'all' ?
+      buildProjectData.phases.reduce((acc, x) => [ ...acc, ...x.entries ], [])
+      : phase.entries
 
     const hours = (entries.reduce((acc, x) => acc + x.minutes, 0) / 60).toFixed(2)
+    const hoursText = `${hours} ${hours === 1 ? 'Hour' : 'Hours'}`
 
-    return `${hours} ${hours === 1 ? 'Hour' : 'Hours'}`
+
+    const entryDates = entries.map(x => x.date).sort()
+
+
+    let timeline
+    const firstDate = entryDates[0]
+    const lastDate = entryDates[entryDates.length - 1]
+    if (!firstDate) {
+      timeline = 'Not Started'
+    } else if (complete) {
+      timeline = `${moment(firstDate).format('D MMMM YYYY')} - ${moment(lastDate).format('DD MMMM YYYY')}`
+    } else {
+      timeline = `${moment(firstDate).format('D MMMM YYYY')} - Ongoing`
+    }
+
+
+
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    })
+
+    const cost = formatter.format(3400.25)
+
+
+    return {
+      hours: hoursText,
+      timeline,
+      cost,
+    }
   }
+  const { hours, timeline, cost } = getHeaderData()
+
+
+  const entries = getEntries()
 
 
 
   const getBuildDetails = async () => {
     const response = await superagent.get(`http://${window.location.hostname}:8081/build-details?buildid=${buildId}`)
     const projectData = JSON.parse(response.text)
-
-    console.log(projectData)
 
     setLoaded(true)
     setLoading(false)
@@ -55,44 +99,60 @@ const BuildProjectPage = () => {
     <div style={{ display: 'flex' }}>
       <PhasesList selection={selectedPhase} phases={buildProjectData.phases} onChange={id => {setSelectedPhase(id)}} />
 
-
       <div style={{ width: '100%' }}>
-
-
         <div style={{ display: 'flex' }}>
           <Paper style={{ padding: '15px', minWidth: '700px' }}>
-
             <div style={{ display: 'flex', position: 'relative' }}>
               <Typography variant='h5'>
                 {buildProjectData.name} {selectedPhase !== 'all' && `(${buildProjectData.phases.find(x => x.id === selectedPhase).name})`}
               </Typography>
-
               <Typography style={{ position: 'absolute', right: '15px' }} variant='h6'>
-                1 August 2020 - 10 August 2020
+                {timeline}
               </Typography>
             </div>
 
             <Divider style={{ marginTop: '10px', marginBottom: '10px' }} />
 
             <Typography variant='h6'>
-              {getHours(selectedPhase)}
+              {hours}
             </Typography>
             <Typography variant='h6'>
-              $3500.01
+              {cost}
             </Typography>
           </Paper>
 
           <Paper style={{ width: '100%', minWidth: '350px', marginLeft: '15px' }}>
             TODO:: Graph display with hours, rivets, expenses over time
           </Paper>
-
         </div>
 
+        {entries.map(entry => {
+          return (
+            <Paper key={JSON.stringify(entry)} style={{ marginTop: '15px', padding: '15px' }}>
+              <div style={{ display: 'flex', position: 'relative' }}>
+                <Typography variant='h6' style={{ width: '150px' }}>
+                  {`${moment(entry.date).format('MMM DD, YYYY')}`}
+                </Typography>
 
+                <Typography variant='h6'>
+                  {`${entry.title} - (${(entry.minutes / 60).toFixed(2)} Hours)`}
+                </Typography>
 
+                <Typography variant='h6' style={{ position: 'absolute', right: '15px' }}>
+                  {entry.phase}
+                </Typography>
+              </div>
 
+              <Divider style={{ marginTop: '10px', marginBottom: '10px' }} />
 
-      </div>  
+              <Typography>
+                {entry.description}
+              </Typography>
+
+            </Paper>
+          )
+        })}
+      </div>
     </div>
   )
 }
